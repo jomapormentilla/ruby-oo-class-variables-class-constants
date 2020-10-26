3395cde27dcbad94e1422d1e9c57eede6889d0b8
jomapormentilla <jomapormentilla@gmail.com>
2020-10-26 14:05:41 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,17 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  # This is a Class Constant, available to all instances of this class
+  GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=( genre )
+    @genre = genre
+    GENRES << genre
   end
 
   def turn_page
